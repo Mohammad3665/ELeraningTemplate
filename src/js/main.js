35a0cc9013a0be9f581ed61a848eac37412eca12
mohammad <mashmammad876@gmail.com>
2026-09-01 03:29:38 +0330

    Implemente login and register pages

--- src/js/main.js
+++ src/js/main.js
@@ -164,128 +164,24 @@ new Swiper(".articles-swiper", {
 
 document.addEventListener("touchstart", function () {}, true);
 
-const gridViewBtn = document.getElementById("grid-view-btn");
-const listViewBtn = document.getElementById("list-view-btn");
-const coursesList = document.getElementById("courses-list");
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".password-wrapper").forEach((wrapper) => {
+    const input = wrapper.querySelector('input[type="password"]');
+    const toggleBtn = wrapper.querySelector(".toggle-password");
+    const eyeIcon = toggleBtn.querySelector(".eye-animated");
 
-const activeButtonClasses = ["bg-primary", "text-white"];
+    toggleBtn.addEventListener("click", function (e) {
+      e.preventDefault();
 
-const inactiveButtonClasses = [
-  "bg-transparent",
-  "text-gray-400",
-  "hover:text-dark",
-];
+      const isPassword = input.type === "password";
 
-function setGridView() {
-  // ساختار container
-  coursesList.classList.remove("flex", "flex-col", "space-y-6");
+      input.type = isPassword ? "text" : "password";
 
-  coursesList.classList.add("grid", "grid-cols-1", "gap-6", "sm:grid-cols-2");
-
-  // تغییر ساختار هر کارت
-  document.querySelectorAll(".course-item").forEach((course) => {
-    course.classList.remove(
-      "sm:flex-row",
-      "gap-5",
-      "border-y",
-      "rounded-tr-2xl",
-      "rounded-br-2xl",
-    );
-
-    course.classList.add(
-      "flex-col",
-      "rounded-2xl",
-      "border",
-      "border-border",
-      "bg-white",
-    );
-
-    // تصویر
-    const imageLink = course.querySelector("a:first-child");
-
-    imageLink.classList.remove("sm:h-auto", "sm:self-stretch", "sm:w-70");
-
-    imageLink.classList.add("h-52", "sm:h-48", "sm:w-full");
-
-    // محتوای کارت
-    const content = course.querySelector("div.flex-1");
-
-    if (content) {
-      content.classList.remove("pt-5");
-
-      content.classList.add("p-5");
-    }
+      if (isPassword) {
+        eyeIcon.classList.add("is-hidden");
+      } else {
+        eyeIcon.classList.remove("is-hidden");
+      }
+    });
   });
-
-  // دکمه Grid فعال
-  gridViewBtn.classList.remove(...inactiveButtonClasses);
-  gridViewBtn.classList.add(...activeButtonClasses);
-
-  // دکمه List غیرفعال
-  listViewBtn.classList.remove(...activeButtonClasses);
-  listViewBtn.classList.add(...inactiveButtonClasses);
-
-  gridViewBtn.setAttribute("aria-pressed", "true");
-  listViewBtn.setAttribute("aria-pressed", "false");
-}
-
-function setListView() {
-  // ساختار container
-  coursesList.classList.remove(
-    "grid",
-    "grid-cols-1",
-    "gap-6",
-    "sm:grid-cols-2",
-  );
-
-  coursesList.classList.add("flex", "flex-col", "space-y-6");
-
-  // تغییر ساختار هر کارت
-  document.querySelectorAll(".course-item").forEach((course) => {
-    course.classList.remove("flex-col", "rounded-2xl", "border", "bg-white");
-
-    course.classList.add(
-      "gap-5",
-      "sm:flex-row",
-      "border-y",
-      "border-border",
-      "rounded-tr-2xl",
-      "rounded-br-2xl",
-    );
-
-    // تصویر
-    const imageLink = course.querySelector("a:first-child");
-
-    imageLink.classList.remove("h-52", "sm:h-48", "sm:w-full");
-
-    imageLink.classList.add(
-      "h-48",
-      "w-full",
-      "sm:h-auto",
-      "sm:self-stretch",
-      "sm:w-70",
-    );
-
-    // محتوای کارت
-    const content = course.querySelector("div.flex-1");
-
-    if (content) {
-      content.classList.remove("p-5");
-      content.classList.add("pt-5");
-    }
-  });
-
-  // دکمه List فعال
-  listViewBtn.classList.remove(...inactiveButtonClasses);
-  listViewBtn.classList.add(...activeButtonClasses);
-
-  // دکمه Grid غیرفعال
-  gridViewBtn.classList.remove(...activeButtonClasses);
-  gridViewBtn.classList.add(...inactiveButtonClasses);
-
-  gridViewBtn.setAttribute("aria-pressed", "false");
-  listViewBtn.setAttribute("aria-pressed", "true");
-}
-
-gridViewBtn.addEventListener("click", setGridView);
-listViewBtn.addEventListener("click", setListView);
+});
